6c941c75c87b89d21f224bbdd8b4f3239ffd85ce
hellosoohello <145718317+hellosoohello@users.noreply.github.com>
2023-11-01 10:11:44 -0400

    add_score

--- sketch.js
+++ sketch.js
@@ -1,36 +1,124 @@
-let boxWidth = 0;
-let boxHeight = 30;
-let isSpaceDown = false;
-let pressStartTime;
+let initialBoxWidth = 0;
+let initialBoxHeight = 20;
+let initialBoxX;
+let initialBoxY;
+let boxWidth;
+let boxHeight;
+let boxX;
+let boxY;
+let isSpaceDownBox = false;
+let pressStartTimeBox;
 let boxColor;
+
+let groundLevel;
+let initialCircleSize = 10;
+let circleSize;
+let initialCircleX;
+let initialCircleY;
+let circleX;
+let circleY;
+let isSpaceDownCircle = false;
+let pressStartTimeCircle;
+let circleColor;
+
+let score = 0;
+
+let hasCollided = false;
 
 function setup() {
   createCanvas(400, 400);
-  boxColor = color(0, 0, 255); // Blue color
+  boxColor = color(0, 0, 255);
+  circleColor = color(255, 255, 0);
+  groundLevel = height - initialBoxHeight;
+  initialBoxX = random(width - initialBoxWidth);
+  initialBoxY = random(groundLevel);
+  boxX = initialBoxX;
+  boxY = initialBoxY;
+  boxWidth = initialBoxWidth;
+  boxHeight = initialBoxHeight;
+  initialCircleX = random(width);
+  initialCircleY = random(groundLevel);
+  circleX = initialCircleX;
+  circleY = initialCircleY;
+  circleSize = initialCircleSize;
 }
 
 function draw() {
   background(220);
-  fill(boxColor);
-  rect(20, 10, boxWidth, boxHeight);
 
-  if (isSpaceDown) {
-    let pressDuration = millis() - pressStartTime;
-    let widthIncrease = map(pressDuration, 0, 1000, 0, 100); // Adjust the map range as needed
-    boxWidth = 20 + widthIncrease;
+  fill(150);
+  rect(0, groundLevel, width, height);
+
+  fill(boxColor);
+  rect(boxX, boxY, boxWidth, boxHeight);
+
+  if (isSpaceDownBox) {
+    let pressDuration = millis() - pressStartTimeBox;
+    let widthIncrease = map(pressDuration, 0, 1000, 0, 100);
+    boxWidth = initialBoxWidth + widthIncrease;
   }
+
+  if (isSpaceDownCircle) {
+    let pressDuration = millis() - pressStartTimeCircle;
+    let jumpHeight = map(pressDuration, 0, 1000, 0, 100);
+    circleY = 370 - jumpHeight * jumpHeight / 100;
+  }
+
+  fill(circleColor);
+  ellipse(circleX, circleY, circleSize, circleSize);
+
+  if (
+    circleY + circleSize >= boxY &&
+    circleY <= boxY + boxHeight &&
+    circleX >= boxX &&
+    circleX <= boxX + boxWidth &&
+    !hasCollided
+  ) {
+    increaseScore();
+    hasCollided = true;
+  } else if (
+    circleY > boxY + boxHeight ||
+    circleY + circleSize < boxY ||
+    circleX > boxX + boxWidth ||
+    circleX + circleSize < boxX
+  ) {
+    hasCollided = false;
+  }
+
+  fill(0);
+  textSize(20);
+  text(`Score: ${score}`, width - 150, 30);
 }
 
 function keyPressed() {
   if (keyCode === 32) {
-    isSpaceDown = true;
-    pressStartTime = millis();
+    isSpaceDownBox = true;
+    pressStartTimeBox = millis();
+    isSpaceDownCircle = true;
+    pressStartTimeCircle = millis();
   }
 }
 
 function keyReleased() {
   if (keyCode === 32) {
-    isSpaceDown = false;
-    boxWidth = 0; // Reset the width to the original size
+    isSpaceDownBox = false;
+    boxWidth = initialBoxWidth;
+    isSpaceDownCircle = false;
   }
+}
+
+function resetGame() {
+  initialBoxX = random(width - initialBoxWidth);
+  initialBoxY = random(groundLevel);
+  boxX = initialBoxX;
+  boxY = initialBoxY;
+  boxWidth = initialBoxWidth;
+  initialCircleX = random(width);
+  initialCircleY = random(groundLevel);
+  circleX = initialCircleX;
+  circleY = initialCircleY;
+}
+
+function increaseScore() {
+  score++;
 }
